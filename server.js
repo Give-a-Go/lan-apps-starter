e3d73061be35cf2235779a6851aff9a781a7f314
Sanat <sanat.thukral@gmail.com>
2025-02-20 23:15:58 +0000

    init commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Serve static files from the "public" directory
+app.use(express.static("public"));
+
+const PORT = 5555;
+
+// Socket.IO connection handling
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  // Example of sending a welcome message to the connected client
+  socket.emit("message", "Welcome to the server!");
+
+  // Example of broadcasting to all clients when someone connects
+  socket.broadcast.emit("message", "A new user joined");
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
